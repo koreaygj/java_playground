21db2d875af1c1291df27cf4d97eedd36fd69ba0
koreaygj <ygj1828@gmail.com>
2023-12-01 16:53:10 +0900

    add: intellji project

--- MyLinkedList/Test/MyLinkedList.java
+++ MyLinkedList/Test/MyLinkedList.java
@@ -3,6 +3,7 @@ package MyLinkedList.Test;
 class Node<E> {
     E data;
     Node<E> next;
+
     Node(E data) {
         this.data = data;
         this.next = null;
@@ -10,90 +11,87 @@ class Node<E> {
 }
 
 public class MyLinkedList<E> {
-  private Node<E> head; // head of the list
+    private Node<E> head; // head of the list
 
-  public MyLinkedList() {
-    this.head = null;
-  }
+    public MyLinkedList() {
+        this.head = null;
+    }
 
-  // Method to add a new node
-  public void add(E data) {
-      Node<E> newNode = new Node<>(data);
-      if (head == null) {
-          head = newNode;
-      } else {
-          Node<E> current = head;
-          while (current.next != null) {
-              current = current.next;
-          }
-          current.next = newNode;
-      }
-  }
+    // Method to add a new node
+    public void add(E data) {
+        Node<E> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
 
-  // get
-  public E get(int index) {
-    if (index < 0 || index >= size()) {
-        throw new IndexOutOfBoundsException();
-    }
-    
-    Node<E> current = head;
-    int count = 0;
-    while (current != null && count < index) {
-        current = current.next;
-        count++;
-    }
-    
-    if (current == null) {
-        throw new IndexOutOfBoundsException();
-    }
-    
-    return current.data;
-}
+    // get
+    public E get(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
 
-  // delete
-  public E remove(int index){
-    if (index < 0 || index >= size()) {
-        throw new IndexOutOfBoundsException();
-    }
-    Node<E> current = head;
-    if (index == 0) {
-        head = head.next;
+        Node<E> current = head;
+        int count = 0;
+        while (current != null && count < index) {
+            current = current.next;
+            count++;
+        }
+
+        if (current == null) {
+            throw new IndexOutOfBoundsException();
+        }
+
         return current.data;
     }
-    int count = 0;
-    while (current != null && count < index - 1) {
-        current = current.next;
-        count++;
+
+    // delete
+    public E remove(int index) {
+        if (index < 0 || index >= size()) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> current = head;
+        if (index == 0) {
+            head = head.next;
+            return current.data;
+        }
+        int count = 0;
+        while (current != null && count < index - 1) {
+            current = current.next;
+            count++;
+        }
+        if (current == null || current.next == null) {
+            throw new IndexOutOfBoundsException();
+        }
+        Node<E> nodeToRemove = current.next;
+        current.next = nodeToRemove.next;
+        return nodeToRemove.data;
     }
-    if (current == null || current.next == null) {
-        throw new IndexOutOfBoundsException();
+
+    // printList
+    public void printList() {
+        Node<E> current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("NULL");
     }
-    Node<E> nodeToRemove = current.next;
-    current.next = nodeToRemove.next;
-    return nodeToRemove.data;
-  }
 
-  // printList
-  public void printList() {
-      Node<E> current = head;
-      while (current != null) {
-          System.out.print(current.data + " -> ");
-          current = current.next;
-      }
-      System.out.println("NULL");
-  }
-
-  // size
-  public int size() {
-    int count = 0;
-    Node<E> current = head;
-    while (current != null) {
-        count++;
-        current = current.next;
+    // size
+    public int size() {
+        int count = 0;
+        Node<E> current = head;
+        while (current != null) {
+            count++;
+            current = current.next;
+        }
+        return count;
     }
-    return count;
-}
-
-
-  // Other methods like remove, find, etc. can be added here
 }
--- MyLinkedList/test.java
+++ MyLinkedList/test.java
@@ -3,28 +3,26 @@ package MyLinkedList;
 import MyLinkedList.Test.MyLinkedList;
 
 public class test {
-  public static void main(String[] args) {
-    MyLinkedList<String> test = new MyLinkedList<>();
-    MyLinkedList<Integer> test2 = new MyLinkedList<>();
-    
-    test.add("test");
-    test.add("gogo");
-    test.add("keep");
-    test2.add(1);
-    test2.add(2);
-    test2.add(3);
+    public static void main(String[] args) {
+        MyLinkedList<String> test = new MyLinkedList<>();
+        MyLinkedList<Integer> test2 = new MyLinkedList<>();
+        test.add("test");
+        test.add("gogo");
+        test.add("keep");
+        test2.add(1);
+        test2.add(2);
+        test2.add(3);
+        test.printList();
+        test2.printList();
 
-    test.printList();
-    test2.printList();
+        System.out.println(test.get(1));
+        System.out.println(test2.get(1));
 
-    System.out.println(test.get(1));
-    System.out.println(test2.get(1));
+        test.remove(1);
+        test2.remove(1);
 
-    test.remove(1);
-    test2.remove(1);
-
-    test.printList();
-    test2.printList();
-  }
+        test.printList();
+        test2.printList();
+    }
 
 }
